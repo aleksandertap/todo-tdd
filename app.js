dbc9d8c3507cc4bafd48bdf06b7624b9b67e7d37
Aleksander <aleksander.tapsi@voco.ee>
2025-10-22 14:32:58 +0300

    Create TodoController.createTodo integration test and fixed unit test for using database

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const todoRoutes = require('./routes/todo.route');
 const app = express();
+const mongodb = require('./mongoDb/mongodb.connect');
+
+mongodb.connect();
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3015, () => {
-  console.log('Server is running on port 3015');
-});
+module.exports = app;
